b27e81206a5c2cb50a42cbedffee0de16cd09184
AndreaScaranari <andrea.scaranari.93@gmail.com>
2023-12-18 16:56:24 +0100

    data_validation_added

--- js/script.js
+++ js/script.js
@@ -1,17 +1,6 @@
-// Prova Check
-// console.log("Hello world!");
-
-// chiedere quantità chilometri da percorrere
-const kmViaggio = parseInt(prompt("Quanti km devi percorrere?", "100"));
-
-// chiedere età al passeggero
-const etaPasseggero = parseInt(prompt("Quanti anni hai?", "30"));
-
-// calcolare tariffa
-// 1. costo al km
+// * Info che ho già all'inizio
+// costo al km
 const kmCost = 0.21;
-// 2. costo per viaggio
-let tripCost = kmViaggio * kmCost;
 
 // scontistica 
 // - 20% per i minorenni
@@ -22,10 +11,31 @@ const underagePrice = 1 - underageDiscount;
 const elderDiscount = 0.4;
 const elderPrice = 1 - elderDiscount;
 
-// Applicazione eventuale sconti
-if (etaPasseggero < 18 || etaPasseggero >= 65) {
-    etaPasseggero < 18 ? tripCost *= underagePrice : tripCost *= elderPrice;
+// * Info da reperire / calcolare
+// chiedere quantità chilometri da percorrere
+const kmViaggio = parseInt(prompt("Quanti km devi percorrere?", "100"));
+
+// chiedere età al passeggero
+const etaPasseggero = parseInt(prompt("Quanti anni hai?", "30"));
+
+// costo per viaggio
+let tripCost = kmViaggio * kmCost;
+
+// data validation
+// invalid condition
+const invalid = isNaN(kmViaggio) || isNaN(etaPasseggero) || kmViaggio <= 0 || etaPasseggero < 0;
+
+// verifica inserimento valori 
+if (invalid) {
+    alert("Errore: per procedere al calcolo della tariffa, inserire valori numerici validi.")
+} else {
+    // Applicazione eventuale sconti
+    if (etaPasseggero < 18 || etaPasseggero >= 65) {
+        etaPasseggero < 18 ? tripCost *= underagePrice : tripCost *= elderPrice;
+    }
+    
+    // Print in page
+    document.getElementById("trip-fee").innerHTML = `Gentile cliente, la tariffa del suo biglietto ammonta a <strong>€ ${tripCost.toFixed(2)}</strong> (al netto di eventuali sconti).`
 }
 
-// Print in page
-document.getElementById("trip-fee").innerHTML = `Gentile cliente, la tariffa del suo biglietto ammonta a <strong>€ ${tripCost}</strong> (al netto di eventuali sconti).`
+
