82110d9691fc3b22ed91340210e9eed9d479987d
AndreaScaranari <andrea.scaranari.93@gmail.com>
2023-12-18 15:51:56 +0100

    prompts_added

---
+++ js/script.js
@@ -0,0 +1,10 @@
+console.log("Hello world!");
+
+// chiedere quantità chilometri da percorrere
+const kmViaggio = parseInt(prompt("Quanti km devi percorrere?", "20"));
+console.log(kmViaggio);
+
+// chiedere età al passeggero
+const etaPasseggero = parseInt(prompt("Quanti anni hai?", "30"));
+console.log(etaPasseggero);
+
