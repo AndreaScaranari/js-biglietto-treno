f99b25cd6822dc37bf5e75c6ce2380e1e05d24bd
AndreaScaranari <andrea.scaranari.93@gmail.com>
2023-12-18 16:20:25 +0100

    discount_applied

--- js/script.js
+++ js/script.js
@@ -1,16 +1,30 @@
-console.log("Hello world!");
+// Prova Check
+// console.log("Hello world!");
 
 // chiedere quantità chilometri da percorrere
-const kmViaggio = parseInt(prompt("Quanti km devi percorrere?", "20"));
-console.log(kmViaggio);
+const kmViaggio = parseInt(prompt("Quanti km devi percorrere?", "100"));
 
 // chiedere età al passeggero
 const etaPasseggero = parseInt(prompt("Quanti anni hai?", "30"));
-console.log(etaPasseggero);
 
 // calcolare tariffa
 // 1. costo al km
 const kmCost = 0.21;
 // 2. costo per viaggio
 let tripCost = kmViaggio * kmCost;
+
+// scontistica 
+// - 20% per i minorenni
+const underageDiscount = 0.2;
+const underagePrice = 1 - underageDiscount;
+
+// - 40% per gli over 65
+const elderDiscount = 0.4;
+const elderPrice = 1 - elderDiscount;
+
+// Applicazione eventuale sconti
+if (etaPasseggero < 18 || etaPasseggero >= 65) {
+    etaPasseggero < 18 ? tripCost *= underagePrice : tripCost *= elderPrice;
+}
+
 console.log(tripCost);
